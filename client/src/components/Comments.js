8ec247f88ab65ff844110fab941020e9eb407cdf
M-a-r-t-i-n-i <martins.bruders@gmail.com>
2022-10-22 22:44:39 +0300

    cleanup

--- client/src/components/Comments.js
+++
@@ -1,5 +0,0 @@
-function Comments(props) {
-  return <input type="text" required={props.required} />;
-}
-
-export default Comments;
--- client/src/components/Heading.js
+++
@@ -1,6 +0,0 @@
-function Heading(props) {
-  const Type = props.headingType;
-  return <Type>{props.headingText}</Type>;
-}
-
-export default Heading;
